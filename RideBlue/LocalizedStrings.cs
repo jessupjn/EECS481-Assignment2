afe2f7db675fc024f8277d8db72fd1c486f682ec
Jackson <jessupjn@umich.edu>
2014-01-23 16:43:46 -0500

    first commit

---
+++ RideBlue/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RideBlue.Resources;
+
+namespace RideBlue
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ RideBlue/MainScreen.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using RideBlue.Resources;
+
+namespace RideBlue
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+
+            //CancelRide.Opacity = 0;
+            //WaitingPos.Opacity = 0;
+            //GetARide.Margin = new Thickness(0, 484, 0, 0);
+
+        }
+
+        private void GetARide(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Get Ride Button Pressed");
+            NavigationService.Navigate(new Uri("/GetRidePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void ChekWaitingPosition(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Waiting Position Button Pressed");
+
+        }
+        private void CancelYourRide(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Cancel Button Pressed");
+
+        }
+        private void SettingsMenu(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Settings Button Pressed");
+            NavigationService.Navigate(new Uri("/SettingsMenuPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void HelpMenu(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Help Button Pressed");
+        }
+
+    }
+}
